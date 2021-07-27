0789d69baf43d67856ff52f96d020575225507f8
thiagoribb <thiribeiro142@gmail.com>
2021-07-27 15:45:23 -0300

    preparando review

--- src/app.ts
+++
@@ -1,7 +0,0 @@
-import { customExpress } from './config/customExpress';
-import { controllers } from './controllers/controllers';
-
-const app = customExpress();
-controllers(app);
-
-export { app };
--- src/controllers/controllers.ts
+++
@@ -1,8 +0,0 @@
-import { recommendations } from "./recommendations/recommendations";
-import { Express } from "express";
-
-const controllers = (app: Express) => {
-    recommendations(app);
-};
-
-export { controllers };
